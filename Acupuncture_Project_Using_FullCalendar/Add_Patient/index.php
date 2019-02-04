cfcb58864955ce265233a2e52ed4e9ff7365875f
Lee44 <jlee7772@yahoo.com>
2019-02-03 18:14:00 -0800

    Added Bootstrap Menu to Add Patient

--- Acupuncture_Project_Using_FullCalendar/Add_Patient/index.php
+++ Acupuncture_Project_Using_FullCalendar/Add_Patient/index.php
@@ -1,11 +1,35 @@
 <!DOCTYPE html>
 <html>    
-    <head>    
-        <title>Registration Form</title>    
-    </head>    
+<head>    
+    <title>Registration Form</title>    
+</head>    
     <body>    
     <link href = "add_patient.css" type = "text/css" rel = "stylesheet" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+    <script src='/lib/jquery-3.3.1.slim.min.js'></script>
+    <script src='/lib/popper.min.js'></script>
+    <script src='/lib/bootstrap.min.js'></script>
+    
+    <nav class="navbar navbar-expand-lg navbar-light bglight">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav justify-content-between">
+                <li class="nav-item"><a class="nav-link" href = "/index.php">Home</a></li>
+                <li class="nav-item"><a class="nav-link active" href="#home">Add Patient</a></li>
+                <li class="nav-item"><a class="nav-link" href = "/Find_Patient/index.php">Find Patient</a></li>
+                <li class="nav-item"><a class="nav-link" href = "/Table_Query/index.php">Show All Data</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    </body>    
+</html>    
+
+<!--
     <div class = "page">
         <div class = "menu topnav">
             <a href = "/index.php">Home</a>
@@ -26,7 +50,7 @@
                         <label>Last Name:</label>    
                         <input type = "text" name = "last_name" value = "" required/>    
                     </div>
-                    <!-- <div class = "form_group">    
+                     <div class = "form_group">    
                         <label>Address:</label>    
                         <input type = "text" name = "Address" value = "" required/>    
                     </div>  
@@ -45,12 +69,12 @@
                     <div class = "form_group">    
                         <label>Phone Number:</label>    
                         <input type = "text" name = "Phone Number" value = "" required/>    
-                    </div>  -->
+                    </div> 
                     <div class = "form_group">    
                         <label>Email:</label>    
                         <input type = "text" name = "email" value = "" required/>    
                     </div> 
-                    <!-- <div class = "form_group">    
+                     <div class = "form_group">    
                         <label>SSN:</label>    
                         <input type = "text" name = "SSN" value = "" required/>    
                     </div> 
@@ -81,7 +105,7 @@
                     <div class = "form_group">    
                         <label>Work Phone:</label>    
                         <input type = "text" name = "Work Phone" value = "" required/>    
-                    </div> --> 
+                    </div>  
                     <div style='margin:25px 0px 25px 0px;padding: 10px;'>    
                         <h1 style='font-size:25px;font-family: Verdana;'>Upload File</h1>
                         <input type="file" name="file" id="fileSelect"/>
@@ -95,8 +119,9 @@
                 </div>    
             </form> 
         </div>
-    </div>
+        </div>
+-->
 
-    </body>    
-</html>    
 
+                    
+                    
