9a26e7970fe34ead092f0a0703a71824a85bf727
Lee44 <jlee7772@yahoo.com>
2019-02-20 16:38:43 -0800

    Edit_Payment: Added a button to add more charges and payments. Charges and payments grabbed from DB are loaded properly into the page. Update button will delete the rows and insert instead of updating cuz a user can add more data and update wouldn't work.

--- Acupuncture_FullCalendar/Payments/Edit_Payment/delete_update_payment.php
+++ Acupuncture_FullCalendar/Payments/Edit_Payment/delete_update_payment.php
@@ -17,45 +17,34 @@ $tax = $_POST['tax'];
 if(empty($co_pay)) $co_pay = 0.00;
 if(empty($tax)) $tax = 0.00;
 
+//var_dump($_POST);
+
+$sql = "DELETE FROM payments WHERE customer_id = '$customer_id' AND charge_id = '$charge_id' ";
+mysqli_query($link, $sql);
+
+$sql2 = "DELETE FROM charges WHERE customer_id = '$customer_id' AND charge_id = '$charge_id' ";
+mysqli_query($link, $sql2);
+
 if(isset($_POST['submit_Update']))
 {
 	for($i = 0; $i < count($_POST['charge']); $i++)
 	{
-		$sql = "UPDATE charges
-		        SET charge_description = '".$_POST['charge_description'][$i]."',
-		            total_charge = '$total_charge',
-		            charge = '".$_POST['charge'][$i]."',
-		            subtotal = '$subtotal',
-			        co_pay = '$co_pay',
-			        tax = '$tax',
-			        charge_note = '".$_POST['charge_note'][$i]."',
-			        charge_created_at = '".$_POST['created_at'][$i]."'		        		 
-			    WHERE customer_id = '$customer_id' AND charge_id = '".$_POST['charge_id']."'";
+		$sql = "INSERT INTO charges (charge_description,total_charge,charge,subtotal,co_pay,tax,customer_id,charge_note,charge_created_at,charge_id) 
+				VALUES ('".$_POST['charge_description'][$i]."','$total_charge','".$_POST['charge'][$i]."','$subtotal','$co_pay','$tax','$customer_id',
+		    	'".$_POST['charge_note'][$i]."','".$_POST['created_at'][$i]."','$charge_id')";
 		mysqli_query($link, $sql);
 	}
 
 	for($i = 0; $i < count($_POST['payment']); $i++)
 	{
-		$sql = "UPDATE payments
-		SET total_payment = '$total_payment',
-		    payment = '".$_POST['payment'][$i]."',
-		    payment_type = '".$_POST['payment_type'][$i]."',
-		    payment_note = '".$_POST['payment_note'][$i]."',
-			payment_created_at = '".$_POST['payment_created_at'][$i]."',
-			balance = '$balance'
-	    WHERE customer_id = '$customer_id' AND charge_id = '".$_POST['charge_id']."'";
+		$sql = "INSERT INTO payments (total_payment,payment,payment_type,customer_id,payment_note,payment_created_at,charge_id,balance) 
+				VALUES ('$total_payment','".$_POST['payment'][$i]."','".$_POST['payment_type'][$i]."','$customer_id',
+		        '".$_POST['payment_note'][$i]."','".$_POST['payment_created_at'][$i]."','$charge_id','$balance')";
 		mysqli_query($link, $sql);
 	}
 }
-else
-{
-	$sql = "DELETE FROM payments WHERE customer_id = '$customer_id' AND charge_id = '$charge_id' ";
-	mysqli_query($link, $sql);
 
-	$sql2 = "DELETE FROM charges WHERE customer_id = '$customer_id' AND charge_id = '$charge_id' ";
-	mysqli_query($link, $sql2);
-}
 mysqli_close($link);
-header("refresh:1;url= http://192.168.1.113:5555/Edit_Patient/index.php");
+header("refresh:1;url= http://192.168.1.136:5555/Edit_Patient/index.php");
 //header("refresh:1;url= http://192.168.1.113:4444/Edit_Patient/index.php");
 ?>
--- Acupuncture_FullCalendar/Payments/Edit_Payment/index.php
+++ Acupuncture_FullCalendar/Payments/Edit_Payment/index.php
@@ -12,14 +12,22 @@ $charge_id = mysqli_real_escape_string($link, $_REQUEST['charge_id']);
 //Charges
 $sql = "SELECT * FROM charges WHERE customer_id = '$customer_id' AND charge_id = '$charge_id'";
 $result = mysqli_query($link,$sql);
-$charges = mysqli_fetch_array($result);
+
+$charges = [];
+while($charges_array = mysqli_fetch_array($result,MYSQLI_ASSOC))
+    array_push($charges,$charges_array);
 
 //Payments
 $sql2 = "SELECT * FROM payments WHERE customer_id = '$customer_id' AND charge_id = '$charge_id'";
 $result2 = mysqli_query($link,$sql2);
-$payments = mysqli_fetch_array($result2);
 
-$_SESSION['charge_id'] = $payments['charge_id'];
+$payments = [];
+while($payments_array = mysqli_fetch_array($result2,MYSQLI_ASSOC))
+    array_push($payments, $payments_array);
+
+//var_dump($payments);
+
+$_SESSION['charge_id'] = $payments[0]['charge_id'];
 
 mysqli_close($link);
 ?>
@@ -106,12 +114,13 @@ mysqli_close($link);
                 </thead>
                 <tbody>
                     <?php
+                    for($i = 0; $i < count($charges); $i++)
                     echo '
-                    <tr class="d-flex">
-                        <td class="col-3"><input type="date"   class="form-control"        name="created_at[]" value="'.$charges['charge_created_at'].'" required></td>
-                        <td class="col-3"><input type="text"   class="form-control"        name="charge_description[]" value="'.$charges['charge_description'].'" required></td>
-                        <td class="col-2"><input type="number" class="form-control charge" name="charge[]" step="any" min="0" max="1000" value="'.$charges['charge'].'" required></td>
-                        <td class="col-4"><input type="text"   class="form-control"        name="charge_note[]" value="'.$charges['charge_note'].'"></td>
+                    <tr class="d-flex" id="charge_row'.($i+1).'">
+                        <td class="col-3"><input type="date"   class="form-control"        name="created_at[]" value="'.$charges[$i]['charge_created_at'].'" required></td>
+                        <td class="col-3"><input type="text"   class="form-control"        name="charge_description[]" value="'.$charges[$i]['charge_description'].'" required></td>
+                        <td class="col-2"><input type="number" class="form-control charge" name="charge[]" step="any" min="0" max="1000" value="'.$charges[$i]['charge'].'" required></td>
+                        <td class="col-4"><input type="text"   class="form-control"        name="charge_note[]" value="'.$charges[$i]['charge_note'].'"></td>
                     </tr>'
                     ?>
                 </tbody>
@@ -121,27 +130,27 @@ mysqli_close($link);
                     <?php
                     echo '
                     <tr class="d-flex">
-                        <td class="col-3"></td>
+                        <td class="col-3"><button type="button" class="btn btn-primary" onclick="add_Charge()">Add Charges</button></td>
                         <td class="col-7 misc_label">SUBTOTAL</td>
                         <td class="col-2">
-                            <input type="text" id="subtotal" class="form-control" name="subtotal" readonly value="$'.$charges['subtotal'].'">
+                            <input type="text" id="subtotal" class="form-control" name="subtotal" readonly value="$'.$charges[0]['subtotal'].'">
                         </td>
                     </tr>
                     <tr class="d-flex">
                         <td class="col-3"></td>
                         <td class="col-7 misc_label">CO PAY</td>
-                        <td class="col-2"><input type="number" id="co_pay" class="form-control charge" name="co_pay" step="any" min="0" max="1000" value="'.$charges['co_pay'].'"></td>
+                        <td class="col-2"><input type="number" id="co_pay" class="form-control charge" name="co_pay" step="any" min="0" max="1000" value="'.$charges[0]['co_pay'].'"></td>
                     </tr>
                     <tr class="d-flex">
                         <td class="col-3"></td>
                         <td class="col-7 misc_label">TAXES</td>
-                        <td class="col-2"><input type="number" id="taxes" class="form-control charge" name="tax" step="any" min="0" max="1000" value="'.$charges['tax'].'"></td>
+                        <td class="col-2"><input type="number" id="taxes" class="form-control charge" name="tax" step="any" min="0" max="1000" value="'.$charges[0]['tax'].'"></td>
                     </tr>
                     <tr class="d-flex">
                         <td class="col-3"></td>
                         <td class="col-7 misc_label">TOTAL</td>
                         <td class="col-2">
-                            <input type="text" id="total_charges" class="form-control" name="total_charge" readonly value="$'.$charges['total_charge'].'">
+                            <input type="text" id="total_charges" class="form-control" name="total_charge" readonly value="$'.$charges[0]['total_charge'].'">
                         </td>
                     </tr>';
                     ?>
@@ -158,12 +167,13 @@ mysqli_close($link);
                 </thead>
                 <tbody>
                     <?php
+                    for($i = 0; $i < count($payments); $i++)
                     echo '
-                    <tr class="d-flex">
-                        <td class="col-3"><input type="date"   class="form-control"        name="payment_created_at[]" value="'.$payments['payment_created_at'].'" required></td>
-                        <td class="col-3"><input type="text"   class="form-control"        name="payment_type[]" maxlength="11" value="'.$payments['payment_type'].'"></td>
-                        <td class="col-2"><input type="number" id="payment" class="form-control charge" name="payment[]" step="any" min="0" max="10000" value="'.$payments['payment'].'"></td>
-                        <td class="col-4"><input type="text"   class="form-control"        name="payment_note[]" value="'.$payments['payment_note'].'"></td>
+                    <tr class="d-flex" id="payment_row'.($i+1).'">
+                        <td class="col-3"><input type="date"   class="form-control"        name="payment_created_at[]" value="'.$payments[$i]['payment_created_at'].'" required></td>
+                        <td class="col-3"><input type="text"   class="form-control"        name="payment_type[]" maxlength="11" value="'.$payments[$i]['payment_type'].'"></td>
+                        <td class="col-2"><input type="number" id="payment" class="form-control charge" name="payment[]" step="any" min="0" max="10000" value="'.$payments[$i]['payment'].'"></td>
+                        <td class="col-4"><input type="text"   class="form-control"        name="payment_note[]" value="'.$payments[$i]['payment_note'].'"></td>
                     </tr>';
                     ?>                 
                 </tbody>
@@ -173,17 +183,17 @@ mysqli_close($link);
                     <?php
                     echo '
                     <tr class="d-flex">
-                        <td class="col-3"></td>
+                        <td class="col-3"><button type="button" class="btn btn-primary" onclick="add_Payment()">Add Payments</button></td>
                         <td class="col-7 misc_label">TOTAL PAYMENT</td>
                         <td class="col-2">
-                            <input type="text" id="total_payment" class="form-control" name="total_payment" readonly value="$'.$payments['total_payment'].'">
+                            <input type="text" id="total_payment" class="form-control" name="total_payment" readonly value="$'.$payments[0]['total_payment'].'">
                         </td>
                     </tr>
                     <tr class="d-flex">
                         <td class="col-3"></td>
                         <td class="col-7 misc_label">BALANCE</td>
                         <td class="col-2">
-                            <input type="text" id="balance" class="form-control" name="balance" readonly value="$'.$payments['balance'].'">
+                            <input type="text" id="balance" class="form-control" name="balance" readonly value="$'.$payments[0]['balance'].'">
                         </td>
                     </tr>';
                     ?>
@@ -196,5 +206,65 @@ mysqli_close($link);
             </div> 
         </form>
     </div>
-    </body>    
+    </body>
+    <script type="text/javascript">
+    function addKeyUpHandlers()
+    {
+        //iterate through each textboxes and add keyup handler to trigger sum event
+        $(".charge").each(function () 
+        {   //Everytime a key goes up the callback function is called
+            $(this).keyup(function()
+            {
+                var subtotal = 0, grand_total = 0, total_payments = 0,payment = 0, payment2 = 0, balance = 0;
+            
+                $(".charge").each(function() 
+                {
+                    //In a method, this refers to the owner object which is the element that has the class charge.
+                    //$(this).attr is still referring to the owner object. attr is a jquery method and you can't get the attribute using this.attr
+                    //$(".charge").attr doesn't work cuz we have 5 elements with class = charge. Thats why you use $(this) since we are cycling thru all 5 elements
+                    if(!isNaN(this.value) && this.value.length !=0 && $(this).attr('id') != 'co_pay' && $(this).attr('id') != 'taxes' && $(this).attr('id') != 'payment' && $(this).attr('id') != 'payment2')
+                        subtotal += parseFloat(this.value);
+                    if(!isNaN(this.value) && this.value.length !=0 && $(this).attr('id') != 'payment' && $(this).attr('id') != 'payment2')
+                        grand_total += parseFloat(this.value);
+                    if(!isNaN(this.value) && this.value.length !=0 && $(this).attr('id') == 'payment')
+                        payment += parseFloat(this.value);
+                    if(!isNaN(this.value) && this.value.length !=0 && $(this).attr('id') == 'payment2')
+                        payment2 += parseFloat(this.value);
+                    
+                });
+                total_payments = payment + payment2;
+                balance = grand_total - total_payments;
+                $("#subtotal").val('$'+subtotal.toFixed(2));
+                $("#total_charges").val('$'+grand_total.toFixed(2));
+                $("#total_payment").val('$'+total_payments.toFixed(2));
+
+                $("#balance").val('$'+balance.toFixed(2));
+            });
+        });
+    }
+    function add_Charge()
+    {
+        var count = $('input[name="charge_description[]"]').length;
+        $('#charge_row'+count).after(
+                                '<tr class="d-flex" id="charge_row'+(count+1)+'">'+
+                                '<td class="col-3"><input type="date"   class="form-control"        name="created_at[]" required></td>'+
+                                '<td class="col-3"><input type="text"   class="form-control"        name="charge_description[]" required></td>'+
+                                '<td class="col-2"><input type="number" class="form-control charge" name="charge[]" step="any" min="0" max="500" required></td>'+
+                                '<td class="col-4"><input type="text"   class="form-control"        name="charge_note[]"></td>'+
+                                '</tr>');
+        addKeyUpHandlers();
+    }
+    function add_Payment()
+    {
+        var count = $('input[name="payment_type[]"]').length;
+        $('#payment_row'+count).after(
+                                '<tr class="d-flex" id="payment_row'+(count+1)+'">'+
+                                '<td class="col-3"><input type="date"   class="form-control"        name="payment_created_at[]" required></td>'+
+                                '<td class="col-3"><input type="text"   class="form-control"        name="payment_type[]" maxlength="11" required></td>'+
+                                '<td class="col-2"><input type="number" id="payment" class="form-control charge" name="payment[]" step="any" min="0" max="1000" required></td>'+
+                                '<td class="col-4"><input type="text"   class="form-control"        name="payment_note[]"></td>'+
+                                '</tr>');
+        addKeyUpHandlers();
+    }
+    </script>      
 </html>    
